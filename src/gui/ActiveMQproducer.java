d515c81b223a5bf1182ae82e1a50d33ee6d24857
John Davis <johnwdavis@gmail.com>
2017-02-20 14:41:25 -0500

    initial commit

---
+++ src/gui/ActiveMQproducer.java
@@ -0,0 +1,48 @@
+package gui;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class ActiveMQproducer
+ */
+public class ActiveMQproducer extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public ActiveMQproducer() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+        PrintWriter	out	=	response.getWriter ();
+		
+		out.println("<html>");
+		
+		out.println("<strong>we will queue a JMS message to an ActiveMQ load-balanced-message-broker-network</strong>");
+		
+		out.println("</html>");
+		
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
